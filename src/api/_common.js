1fea900352d8622ffe2ca80f51a7b249123e517c
Johann Morales <jm@Johanns-MacBook-Pro.local>
2025-01-21 12:59:16 -0500

    update

--- src/api/_common.js
+++ src/api/_common.js
@@ -8,8 +8,19 @@ async function http(url, init) {
     method: "GET",
     ...(init ?? {}),
     headers: {
-      accept: "application/json",
-      "cache-control": "max-age=0",
+      headers: {
+        accept: "*/*",
+        "cache-control": "max-age=0",
+        "accept-language": "en-US,en;q=0.9",
+        priority: "u=1, i",
+        "sec-ch-ua": '"Not A(Brand";v="8", "Chromium";v="132"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        ...(init?.headers ?? {}),
+      },
       ...(init?.headers ?? {}),
     },
   });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const { getProfile } = require("./api/get-profile.js");
-const { getChannelUserProfile } = require("./api/get-channel-user-profile.js");
 const { ban } = require("./api/ban.js");
-const { unmod } = require("./api/unmod.js");
 const { chat } = require("./api/chat.js");
+const { getChannelUserProfile } = require("./api/get-channel-user-profile.js");
+const { getProfile } = require("./api/get-profile.js");
 const { mod } = require("./api/mod.js");
+const { unmod } = require("./api/unmod.js");
 const { joinChatroom } = require("./ws/join-chatroom.js");
 
 module.exports = {
