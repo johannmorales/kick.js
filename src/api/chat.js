f3306ae2b145dbd99c6eedd0edbebf20f26dc5d4
Johann Morales <jm@Johanns-MacBook-Pro.local>
2025-03-27 10:01:57 -0500

    fix chat headers

--- src/api/chat.js
+++ src/api/chat.js
@@ -4,6 +4,7 @@ async function chat(authorizationToken, channelId, message) {
   await http(`https://kick.com/api/v2/messages/send/${channelId}`, {
     headers: {
       authorization: authorizationToken,
+      "content-type": "application/json",
     },
     body: `{"content":"${message}","type":"message"}`,
     method: "POST",
